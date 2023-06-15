bcad55497373307c705085fdd83e1f1c5c78e5bc
Sharif Eari <33998022+sharif3271@users.noreply.github.com>
2023-06-15 21:58:05 +0300

    add a function for handling creating of branches

--- git-gen.plugin.zsh
+++ git-gen.plugin.zsh
@@ -1,1 +1,25 @@
 # Creates bulk branches and delete them
+function make_branches() {
+  if [[ ! -d "${PWD}/.git" ]]; then
+    echo 'This directory does not contain .git';
+    return;
+  fi
+  exists=`git show-ref refs/heads/$1`
+  if [ ! -n "$exists" ]; then
+    echo "There is no branch named: $1";
+    return;
+  else
+    git checkout $1;
+  fi
+  
+  arr=("$@");
+  arraylength=${#arr[@]}
+  for (( i=2; i<=${arraylength}; i++ ));
+  do
+     bn="${arr[$i]}"
+     exists=`git show-ref refs/heads/$bn`
+     if [ ! -n "$exists" ]; then
+       git branch $bn
+     fi
+  done
+}
